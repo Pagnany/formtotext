56deb08e216fb635d2356f5bddfd51e417635e1a
Eric Schmale <eric.schmale@gmx.de>
2023-06-11 12:34:19 +0200

    Umgeschrieben mit ? Notation.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use filetime::{set_file_mtime, FileTime};
 use std::env;
 use std::fs;
+use std::io;
+use std::io::ErrorKind;
 use std::path::PathBuf;
 use std::process::Command;
 use std::time::UNIX_EPOCH;
@@ -19,69 +21,61 @@ fn main() {
     let paths = fs::read_dir(repo_forms_pfad).expect("Pfad konnte nicht gelesen werden.");
 
     for path in paths {
-        let temp_path_scx = match path {
-            Ok(wert) => wert.path(),
-            _ => continue,
-        };
-
-        // Endung von der Datei holen und prüfen
-        match temp_path_scx.extension() {
-            Some(wert) => {
-                if wert.to_ascii_lowercase() != "scx" {
-                    continue;
-                }
-            }
-            _ => continue,
-        };
-
-        // Hier haben wir nur Dateien mit einer scx Endung
-
-        // Pfad zur sc2 Datei bauen
-        let mut temp_path_sc2 = temp_path_scx.clone();
-        temp_path_sc2.set_extension("sc2");
-
-        let time_now = FileTime::now();
-        // gibt es die Datei sc2
-        if std::path::Path::new(&temp_path_sc2).exists() {
-            // Hier das modification Date prüfen
-            if get_last_edit_time(&temp_path_scx) != get_last_edit_time(&temp_path_sc2) {
-                // Datei löschen
-                match fs::remove_file(&temp_path_sc2) {
-                    Ok(_wert) => (),
-                    _ => ()
-                }
-                // Neue SC2 Datei erstellen
-                match Command::new(&repo_exe_pfad).arg(&temp_path_scx).output() {
-                    Ok(_wert) => (),
-                    _ => ()
-                }
-                // Die Letzte Bearbeitungsezeit setzten
-                match set_file_mtime(&temp_path_sc2, time_now) {
-                    Ok(_wert) => (),
-                    _ => {continue;}
-                }
-                match set_file_mtime(&temp_path_scx, time_now) {
-                    Ok(_wert) => (),
-                    _ => {continue;}
-                }
-            }
-        } else {
-            // Neue SC2 Datei erstellen
-            match Command::new(&repo_exe_pfad).arg(&temp_path_scx).output() {
-                Ok(_wert) => (),
-                _ => (),
-            }
-            // Die Letzte Bearbeitungsezeit setzten
-            match set_file_mtime(&temp_path_sc2, time_now) {
-                Ok(_wert) => (),
-                _ => {continue;}
-            }
-            match set_file_mtime(&temp_path_scx, time_now) {
-                Ok(_wert) => (),
-                _ => {continue;}
-            }
+        match check_and_create_sc2_file(path, &repo_exe_pfad) {
+            Ok(_) => {}
+            Err(_e) => continue,
         }
     }
+}
+
+fn check_and_create_sc2_file(
+    path: Result<fs::DirEntry, io::Error>,
+    repo_exe_pfad: &String,
+) -> Result<(), io::Error> {
+    let temp_path_scx = path?.path();
+
+    // Endung von der Datei holen und prüfen
+    match temp_path_scx.extension() {
+        Some(wert) => {
+            if wert.to_ascii_lowercase() != "scx" {
+                return Err(io::Error::new(
+                    ErrorKind::Other,
+                    "Datei hat nicht die richtige Endung",
+                ));
+            }
+        }
+        None => return Err(io::Error::new(ErrorKind::Other, "Datei hat keine Endung")),
+    };
+
+    // Hier haben wir nur Dateien mit einer scx Endung
+
+    // Pfad zur sc2 Datei bauen
+    let mut temp_path_sc2 = temp_path_scx.clone();
+    temp_path_sc2.set_extension("sc2");
+
+    let time_now = FileTime::now();
+    // gibt es die Datei sc2
+    if std::path::Path::new(&temp_path_sc2).exists() {
+        // Hier das modification Date prüfen
+        if get_last_edit_time(&temp_path_scx) != get_last_edit_time(&temp_path_sc2) {
+            // Datei löschen
+            fs::remove_file(&temp_path_sc2)?;
+
+            // Neue SC2 Datei erstellen
+            Command::new(&repo_exe_pfad).arg(&temp_path_scx).output()?;
+            // Die Letzte Bearbeitungsezeit setzten
+            set_file_mtime(&temp_path_sc2, time_now)?;
+            set_file_mtime(&temp_path_scx, time_now)?;
+        }
+    } else {
+        // Neue SC2 Datei erstellen
+        Command::new(&repo_exe_pfad).arg(&temp_path_scx).output()?;
+        // Die Letzte Bearbeitungsezeit setzten
+        set_file_mtime(&temp_path_sc2, time_now)?;
+        set_file_mtime(&temp_path_scx, time_now)?;
+    }
+
+    Ok(())
 }
 
 fn get_last_edit_time(pfad: &PathBuf) -> u64 {
